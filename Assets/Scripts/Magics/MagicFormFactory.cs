f6cf4481a661002deab1570e4ae9463a66022bc4
元プログラマi <99943229+moto-programmer-i@users.noreply.github.com>
2025-05-06 23:44:56 +0900

    〇ボタンで魔法がでる

---
+++ Assets/Scripts/Magics/MagicFormFactory.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MagicFormFactory : MonoBehaviour
+{
+    [SerializeField]
+    private float cooltime = 1.0f;
+
+    
+    /// <summary>
+    /// 魔法陣の入力
+    /// </summary>
+    [SerializeField]
+    private InputAction cast;
+
+    [SerializeField]
+    private GameObject magicForm;
+
+    /// <summary>
+    /// Actionの有効を自分で管理するクラス
+    /// </summary>
+    private readonly List<InputAction> actions = new ();
+
+    // 最後に発動した時間
+    private float lastCastTime = float.MinValue;
+
+    void Start()
+    {
+        // 魔法陣入力設定
+        cast.performed += context => {
+            // 固定クールタイム
+            if(Time.time - lastCastTime < cooltime) {
+                return;
+            }
+            lastCastTime = Time.time;
+
+            Instantiate(magicForm, transform.position, transform.rotation);
+        };
+    }
+
+    // InputActionはOnEnableとOnDisableを実装しなければならない
+    // https://docs.unity3d.com/Packages/com.unity.inputsystem@1.14/manual/Actions.html#enabling-actions
+    void OnEnable()
+    {
+        // アクションが登録されていなければ登録する
+        if (!actions.Any()) {
+            if (cast != null) {
+                actions.Add(cast);
+            }
+            
+        }
+        
+        actions.ForEach(action => action.Enable());
+    }
+
+    void OnDisable()
+    {
+        actions.ForEach(action => action.Disable());
+    }
+
+}
