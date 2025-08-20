bbebe833afef4047979e9dd742130714e7ef6c24
元プログラマi <99943229+moto-programmer-i@users.noreply.github.com>
2025-08-20 17:27:56 +0900

    チュートリアルの内容のコメント追加

--- BeMighty.cpp
+++ BeMighty.cpp
@@ -30,8 +30,10 @@ void recordCommandBufferToTutorial(Vulkan::Vulkan& vulkan, uint32_t imageIndex) 
     );
 
 
-    // この辺のマジックナンバーも要調査
-    vk::ClearValue clearColor = vk::ClearColorValue(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    vk::ClearValue clearColor = vk::ClearColorValue(
+        // 黒色
+        0.0f, 0.0f, 0.0f, 1.0f);
     vk::RenderingAttachmentInfo attachmentInfo = {
         .imageView = vulkan.getSwapChain().getSwapChainImageViews()[imageIndex],
         .imageLayout = vk::ImageLayout::eColorAttachmentOptimal,
@@ -39,9 +41,13 @@ void recordCommandBufferToTutorial(Vulkan::Vulkan& vulkan, uint32_t imageIndex) 
         .storeOp = vk::AttachmentStoreOp::eStore,
         .clearValue = clearColor
     };
+
+    // チュートリアル用のコードなので、マジックナンバーでも許容
     vk::RenderingInfo renderingInfo = {
         .renderArea = {.offset = { 0, 0 }, .extent = vulkan.getSwapChain().getSwapChainExtent()},
         .layerCount = 1,
+
+        // 本来は配列と要素数だが、1個しかないのでこう書くしかない
         .colorAttachmentCount = 1,
         .pColorAttachments = &attachmentInfo
     };
@@ -50,9 +56,40 @@ void recordCommandBufferToTutorial(Vulkan::Vulkan& vulkan, uint32_t imageIndex) 
     commandBuffer.beginRendering(renderingInfo);
     commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, *vulkan.getGraphicsPipeline().getGraphicsPipeline());
     commandBuffer.setViewport(0, vk::Viewport(0.0f, 0.0f, static_cast<float>(vulkan.getSwapChain().getSwapChainExtent().width), static_cast<float>(vulkan.getSwapChain().getSwapChainExtent().height), 0.0f, 1.0f));
+
+    // 領域を指定
+    // https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/03_Drawing/01_Command_buffers.html#_basic_drawing_commands
     commandBuffer.setScissor(0, vk::Rect2D(vk::Offset2D(0, 0), vulkan.getSwapChain().getSwapChainExtent()));
-    commandBuffer.draw(3, 1, 0, 0);
+
+
+
+
+
+    
+    
+    
+    
+    commandBuffer.draw(
+        // vertexCount: Even though we don’t have a vertex buffer, we technically still have 3 vertices to draw.
+        // 現状はshader.slangのpositionの数と合わせないとうまく動作しない
+        3,
+
+        // instanceCount : Used for instanced rendering, use 1 if you’re not doing that.
+        1,
+
+        // firstVertex : Used as an offset into the vertex buffer, defines the lowest value of SV_VertexId.
+        0,
+
+        // firstInstance : Used as an offset for instanced rendering, defines the lowest value of SV_InstanceID.
+        0
+    );
+
+
+
+
     commandBuffer.endRendering();
+
+
     // After rendering, transition the swapchain image to PRESENT_SRC
     vulkan.getCommand().transitionImageLayout(
         imageIndex,
